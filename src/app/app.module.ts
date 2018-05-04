4ce1376493495348cc3920e10e0f712b7ae37bc7
anurag gupta <anurag@Samads-MacBook-Pro.local>
2018-05-04 12:14:07 +0530

    caurosel using seperate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MainImageComponent } from './main-image/main-image.component';
+import { ImageServiceService } from './services/image-service.service';
+import { SelectedImageComponent } from './selected-image/selected-image.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainImageComponent,
+    SelectedImageComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ImageServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
